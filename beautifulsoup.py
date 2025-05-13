2100026e7d7c8354a2e1cf964027e1a4b1c618af
hajotkaur <harjotkaur2113@gmail.com>
2025-05-13 13:51:49 -0600

    update

---
+++ beautifulsoup.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.service import Service
+import csv
+
+
+#initialize the chrome web driver
+driver = webdriver.chrome()
+
+page_url = 'https://ww150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1810000401'
+
+
+#fetching  the web page 
+driver.get(page_url)
+
+
+# Wair for the table to load
+driver.implicity_wait(10)
+
+# Get the page source after javaScript execution 
+page_source = driver.page_source
+
+# Parse the HTML content
+soup = BeautifulSoup(page_source, 'html.parser')
+
+# Find the table element 
+table = soup.find(id='simpleTable')
+
+if table:
+    #Extract column headers
+    headers = []
+
+
+
+
+
+import requests 
+
+#fetching webpage content
+url = 'https://ww150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1810000401'
+response = requests.get(url)
+html_content = response.content
+
+#Parse the html content BeautifulSoup 
+soup = BeautifulSoup(html_content, 'html.parser')
+
+#extract specific elements 
+table = soup.find(id='simpleTable')
+
+print(table)
+
---
+++ python_examples.py
@@ -0,0 +1,46 @@
+# this is a comment
+x = 5
+print(x)
+
+
+# this is an example of a tuple 
+mytuple =('honda','toyota', 'ford')
+
+#lists (arrays)
+list = ['apple''cherry','orange']
+
+#data dictionaries
+dictionary = {'todd': 38,'janet':60,'Bob': 90}
+
+#if-else conditional logic
+a = 3
+b = 220
+if a > b:
+    print('a is greater than b')
+elif a < b:
+    print('b is greater than a')
+else:
+    print('a is equal to b')
+
+#looping in python 
+colours = ['red','green','grey','blue']
+for x in colours:
+    print(x)
+
+  #while loop
+
+i = 1
+while i < 10:
+    print(i)
+    i += 1
+
+#functions 
+def add(a,b):
+    return a + b
+
+print(add(14, 5))
+
+#casting
+x = int(1)
+b = float(2.33)
+v = str(23)
